c680841755517c94f7cbcdd9a92fbb17f961641c
Chris Nyaga <nyagachris75@gmail.com>
2017-06-16 14:57:26 +0300

    New restaurant template updated and added appropriate actions

--- app/components/new-restaurant.js
+++ app/components/new-restaurant.js
@@ -1,4 +1,62 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewRestaurant: false,
+  actions:{
+    restaurantFormShow(){
+      this.set('addNewRestaurant',true)
+    },
+    cancel(){
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('description',''),
+      this.set('dateOpened',''),
+      this.set('website',''),
+      this.set('phone',''),
+      this.set('locationName',''),
+      this.set('locationLongitude',''),
+      this.set('locationLongitude',''),
+      this.set('addNewRestaurant',false)
+    },
+    reset(){
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('description',''),
+      this.set('dateOpened',''),
+      this.set('website',''),
+      this.set('phone',''),
+      this.set('locationName',''),
+      this.set('locationLongitude',''),
+      this.set('locationLongitude','')
+    },
+    saveNewRestaurant(){
+      var params = {
+        name: this.get('name'),
+        category: this.get('category'),
+        shortDescription: this.get('shortDescription'),
+        description: this.get('description'),
+        dateOpened: this.get('dateOpened'),
+        website: this.get('website'),
+        phone: this.get('phone'),
+        locationName: this.get('locationName'),
+        locationLongitude: this.get('locationLongitude'),
+        locationLatitude: this.get('locationLatitude'),
+      };
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('description',''),
+      this.set('dateOpened',''),
+      this.set('website',''),
+      this.set('phone',''),
+      this.set('locationName',''),
+      this.set('locationLongitude',''),
+      this.set('locationLongitude',''),
+      this.set('addNewRestaurant',false),
+      this.sendAction('saveNewRestaurant',params),
+      this.transitionTo('index')
+    }
+  }
 });

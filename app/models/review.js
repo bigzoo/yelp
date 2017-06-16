c1de48217d98e8408888fc8144726b48ebd9e72d
Chris Nyaga <nyagachris75@gmail.com>
2017-06-16 11:41:42 +0300

    Review model defined

--- app/models/review.js
+++ app/models/review.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  description: DS.attr(),
+  rating: DS.attr('number'),
+  timeCreated: DS.attr(),
+  restaurant: DS.belongsTo('restaurant',{async:true})
 
 });

03487b9c2fa8251628c3628efa137a235cc966de
Chris Nyaga <nyagachris75@gmail.com>
2017-06-16 11:38:38 +0300

    Restaurant model defined

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  name: DS.attr(),
+  category: DS.attr(),
+  shortDescription: DS.attr(),
+  description: DS.attr(),
+  dateOpened: DS.attr(),
+  website: DS.attr(),
+  phone: DS.attr(),
+  locationName: DS.attr(),
+  locationLongitude: DS.attr(),
+  locationLatitude: DS.attr(),
+  reviews: DS.hasMany('review',{async:true})
 
 });

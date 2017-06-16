87c4ed701195e83e7396c951968690e259f74232
Chris Nyaga <nyagachris75@gmail.com>
2017-06-16 16:07:15 +0300

    New review actions added.

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
+import moment from 'moment'
 
 export default Ember.Component.extend({
+  addNewReview: false,
+  actions:{
+    reviewFormShow(){
+      this.set('addNewReview',true)
+    },
+    cancel(){
+      this.set('description',''),
+      this.set('rating',''),
+      this.set('addNewReview',false)
+    },
+    reset(){
+      this.set('description',''),
+      this.set('rating','')
+    },
+    saveNewReview(){
+      var params = {
+        description: this.get('description'),
+        rating: this.get('rating'),
+        timeCreated: Date(),
+        review: this.get('restaurant')
+      };
+      this.set('description',''),
+      this.set('rating',''),
+      this.set('addNewReview',false),
+      this.sendAction('saveNewReview',params)
+    }
+  }
 });

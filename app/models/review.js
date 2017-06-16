82054cd76a64a8f7928bc180bfa6c8e679062205
Chris Nyaga <nyagachris75@gmail.com>
2017-06-16 15:39:07 +0300

    New review component template updated and review model updated.

--- app/models/review.js
+++ app/models/review.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr(),
-  rating: DS.attr('number'),
+  rating: DS.attr(),
   timeCreated: DS.attr(),
   restaurant: DS.belongsTo('restaurant',{async:true})
 
